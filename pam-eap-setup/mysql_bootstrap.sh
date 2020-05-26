94f05f26eb1a78685871e8b9e7a48b8261fe333c
Marcelo Sales <marcelo.d.sales@gmail.com>
2020-05-26 16:19:20 -0300

    update project

--- pam-eap-setup/mysql_bootstrap.sh
+++ pam-eap-setup/mysql_bootstrap.sh
@@ -7,9 +7,9 @@ yum update -y
 yum install mysql-server
 systemctl start mysqld
 
-mysql CREATE USER 'pamAdmin'@'%' IDENTIFIED BY 'S3cr3tK3y#';
-mysql GRANT ALL ON *.* TO 'pamAdmin'@'%' IDENTIFIED BY 'S3cr3tK3y#';
-mysql CREATE DATABASE COMGAS;
+#mysql CREATE USER 'pamAdmin'@'%' IDENTIFIED BY 'S3cr3tK3y#';
+#mysql GRANT ALL ON *.* TO 'pamAdmin'@'%' IDENTIFIED BY 'S3cr3tK3y#';
+#mysql CREATE DATABASE COMGAS;
 
 yum --enablerepo=epel -y install phpMyAdmin php-mysql php-mcrypt
 systemctl restart httpd
