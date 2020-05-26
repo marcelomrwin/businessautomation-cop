d67e1981b1518da38de1de3e9ee90de309ec4604
Marcelo Sales <marcelo.d.sales@gmail.com>
2020-05-26 16:35:41 -0300

    update project

--- pam-eap-setup/mysql_bootstrap.sh
+++ pam-eap-setup/mysql_bootstrap.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 yum -y install wget
+yum -y install epel-release
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
 yum update -y
 
-yum install mysql-server
+yum -y install mysql-server
 systemctl start mysqld
 
 #mysql CREATE USER 'pamAdmin'@'%' IDENTIFIED BY 'S3cr3tK3y#';
 #mysql GRANT ALL ON *.* TO 'pamAdmin'@'%' IDENTIFIED BY 'S3cr3tK3y#';
 #mysql CREATE DATABASE COMGAS;
 
-yum --enablerepo=epel -y install phpMyAdmin php-mysql php-mcrypt
+yum -y install httpd
+yum -y install phpmyadmin
 systemctl restart httpd
