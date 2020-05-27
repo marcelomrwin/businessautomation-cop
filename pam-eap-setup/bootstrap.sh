39aa1eaac8a458aab296c077044816807e771db7
Marcelo Sales <marcelo.d.sales@gmail.com>
2020-05-27 09:54:52 -0300

    update project

---
+++ pam-eap-setup/bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+yum -y install wget
+yum -y install epel-release
+yum -y update
+yum -y install unzip
+yum -y install maven
+yum -y install git
+yum -y install java-1.8.0-openjdk-devel
+
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum update -y
+
+yum -y install mysql-server
+systemctl start mysqld
+
+#mysql CREATE USER 'pamAdmin'@'%' IDENTIFIED BY 'S3cr3tK3y#';
+#mysql GRANT ALL ON *.* TO 'pamAdmin'@'%' IDENTIFIED BY 'S3cr3tK3y#';
+#mysql CREATE DATABASE COMGAS;
+
+yum -y install httpd
+yum -y install phpmyadmin
+systemctl restart httpd
+
+cd /vagrant && ./pam-setup.sh -n localhost:8080 -b both
+chown -R vagrant:vagrant /vagrant
+nohup sh /vagrant/go_pam.sh &
